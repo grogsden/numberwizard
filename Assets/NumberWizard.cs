4283877be6a7bfcc843fe2de9ae966a8b31ad2ac
grogsden <brian.stempien@protonmail.com>
2021-06-29 20:41:28 -0500

    refactored the code and added score

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,9 +7,17 @@ public class NumberWizard : MonoBehaviour
     int max = 1000;
     int min = 1;
     int guess = 500;
+    int score = 0;
 
     // Start is called before the first frame update
     void Start()
+    {
+        StartGame();
+
+        
+    }
+
+    void StartGame()
     {
         Debug.Log("Welcome to NumberWizard, player.");
         Debug.Log("Choose a number, player. But keep it to yourself.");
@@ -18,8 +26,6 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Tell me if your number is higher or lower than: " + guess + ".");
         Debug.Log("Push Up Arrow if it's higher, or push Down Arrow if it's lower, push Enter if the guess is correct.");
         max = max + 1;
-
-        
     }
 
     // Update is called once per frame
@@ -27,23 +33,25 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Your number is higher than " + guess + ".");
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it..." + guess + "?");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Your number is lower than " + guess +".");
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it..." + guess + "?");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Your number is " + guess +"!");
-        }
-        
-
+            Debug.Log("It only took me " + score + " tries!");
+        }   
     }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is your number higher or lower than " + guess + ".");
+        score = score +1;
+    }
+
 }
